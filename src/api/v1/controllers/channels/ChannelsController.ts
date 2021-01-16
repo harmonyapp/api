d4bae04f84bb39eab790c1722bec254dc3bec054
chris9740 <christofferhja@gmail.com>
2021-01-17 00:57:41 +0100

    refactor: remove unused controller functions

--- src/api/v1/controllers/channels/ChannelsController.ts
+++ src/api/v1/controllers/channels/ChannelsController.ts
@@ -1,40 +1,5 @@
-import { Response, Request, NextFunction } from "express";
 import BaseController from "../BaseController";
-import HttpStatusCode from "../../../../interfaces/HttpStatusCode";
-import Channel from "../../../../models/channel";
-import GenericError from "../../../../errors/GenericError";
-import ErrorMessages from "../../../../errors/Messages";
-import { ControllerReturnPromise } from "../../../../interfaces/ControllerReturn";
 
-class ChannelsController extends BaseController {
-    public static async getChannels(req: Request, res: Response): ControllerReturnPromise {
-        const channels = await Channel.find({ server: req.bus.server.id }).sort("position");
-
-        return res.send({ channels });
-    }
-
-    public static async getChannelByID(req: Request, res: Response, next: NextFunction): ControllerReturnPromise {
-        const channel = await Channel.findOne({ _id: req.params.channelID, server: req.bus.server.id });
-
-        if (!channel) {
-            return next(new GenericError("Channel not found").setHttpStatusCode(HttpStatusCode.NOT_FOUND));
-        }
-
-        return res.send({ channel });
-    }
-
-    public static async deleteChannel(req: Request, res: Response, next: NextFunction): ControllerReturnPromise {
-        const server = req.bus.server;
-        const channel = req.bus.channel;
-
-        try {
-            await channel.remove();
-        } catch (error) {
-            return next(error);
-        }
-
-        return res.send({ channel });
-    }
-}
+class ChannelsController extends BaseController { }
 
 export default ChannelsController;
