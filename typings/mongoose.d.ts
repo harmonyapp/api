81274504b49c18b02630e3a26b6f129d6bffa310
chris9740 <christofferhja@gmail.com>
2021-03-02 23:28:03 +0100

    docs: document the function of the getPresentableObject methods

--- typings/mongoose.d.ts
+++ typings/mongoose.d.ts
@@ -7,18 +7,36 @@ declare module "mongoose" {
     export type PresentableFieldValue = boolean | {
         populate: boolean;
         yield?: string;
-    };
+    } | ((...options: unknown[]) => boolean);
 
     export type PresentableField<T = null> = PartialRecord<PresentableFieldKey<T>, PresentableFieldValue>;
 
     interface Document {
+        /**
+         * Returns the presentable object, populated and cleaned up, ready to be sent to the client
+         */
         getPresentableObject(): Record<string, unknown>;
+        /**
+         * Returns all fields that are subject to being populated, if any
+         */
         getPopulateableFields(): string[];
+        /**
+         * Configure a single presentable field.
+         * <note>This applies only to the single document it's run on</note>
+         */
         setPresentableField(key: PresentableFieldKey<this> | string, value: PresentableFieldValue): Document;
+        /**
+         * Configure the presentable fields for the document.
+         * <note>This only overwrites the fields that were provided, the others remain intact.</note>
+         * <note>This applies only to the single document it's run on</note>
+         */
         setPresentableFields(fields: PresentableField<this>): Document;
     }
 
     interface Model<T extends Document, QueryHelpers = {}> {
+        /**
+         * Configure the presentable fields for this model
+         */
         setPresentableFields(fields: PresentableField<T>): void;
     }
 }
