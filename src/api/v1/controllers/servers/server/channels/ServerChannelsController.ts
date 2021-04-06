07534ab2546c0d8c55f4fdf4ee84b1e4bb1b7c31
chris9740 <christofferhja@gmail.com>
2021-04-06 08:12:55 +0200

    feat(Channels): allow for channels to be moved between categories

--- src/api/v1/controllers/servers/server/channels/ServerChannelsController.ts
+++ src/api/v1/controllers/servers/server/channels/ServerChannelsController.ts
@@ -3,7 +3,7 @@ import { matchedData } from "express-validator";
 import BaseController from "../../../BaseController";
 import HttpStatusCode from "../../../../../../interfaces/HttpStatusCode";
 import ErrorMessages from "../../../../../../errors/Messages";
-import Channel from "../../../../../../models/channel";
+import Channel, { IChannelDocument } from "../../../../../../models/channel";
 import FieldError from "../../../../../../errors/FieldError";
 import { ControllerReturnPromise } from "../../../../../../interfaces/ControllerReturn";
 import { ChannelTypes } from "../../../../../../util/Constants";
@@ -67,31 +67,45 @@ class ServerChannelsController extends BaseController {
     }
 
     public static async updateChannels(req: Request, res: Response, next: NextFunction): ControllerReturnPromise {
-        const channelData: { id: string; position: number; }[] = req.body;
+        const channelData: { id: string; position?: number; parent?: string | null; }[] = req.body;
         const server = req.bus.server;
+
+        const channelIDsArray = channelData.map((channel) => channel.id);
+        const duplicateIDs = channelIDsArray.some((id, index) => channelIDsArray.indexOf(id) !== index);
+
+        if (duplicateIDs) {
+            return next(new GenericError("Duplicate ID's found"));
+        }
 
         if (!Array.isArray(channelData)) {
             return next(new GenericError("This endpoint expects an array"));
         }
 
-        if (channelData.length < 2) {
-            return next(new GenericError("You cannot edit less than 2 channels"));
-        }
+        const serverChannels = await Channel.find({ server: server.id });
 
         for (const channel of channelData) {
-            const missingID = !Object.prototype.hasOwnProperty.call(channel, "id");
-            const missingPosition = !Object.prototype.hasOwnProperty.call(channel, "position");
-
-            if (missingID || missingPosition) {
-                return next(new FieldError(missingID ? "id" : "position", "This field is required"));
+            if (!channel.id) {
+                return next(new FieldError("id", "This field is required"));
             }
 
-            if (!isNumeric(channel.position)) {
+            if (channel.position && !isNumeric(channel.position)) {
                 return next(new FieldError("position", "This field expects a number"));
             }
-        }
 
-        const serverChannels = await Channel.find({ server: server.id });
+            if (Object.prototype.hasOwnProperty.call(channel, "parent")) {
+                if (channel.parent !== null) {
+                    const parentChannel = serverChannels.find((ch) => ch.id === channel.parent);
+
+                    if (!parentChannel) {
+                        return next(new FieldError("parent", "Parent channel not found"));
+                    }
+
+                    if (parentChannel.type !== ChannelTypes.SERVER_CATEGORY) {
+                        return next(new FieldError("parent", "Parent channel must be a category"));
+                    }
+                }
+            }
+        }
 
         const affectedChannels = serverChannels.filter((channel) => {
             return channelData.findIndex((data) => {
@@ -103,31 +117,75 @@ class ServerChannelsController extends BaseController {
             return next(new GenericError("One or more of the provided channel ID's are not valid"));
         }
 
-        if (!affectedChannels.every((channel) => channel.parent === affectedChannels[0].parent)) {
-            return next(new GenericError("All channels must be in the same category"));
-        }
-
         if (!affectedChannels.every((channel) => channel.isSwappableWith(affectedChannels[0].type))) {
             return next(new GenericError("You can't mix category channels with text/voice channels"));
+        }
+
+        // If the user provides a parent channel but not a position, we will assign the lowest position (highest number).
+        // The first time we set the position of a channel that wasn't given one, we use the channel count to calculate the position.
+        // Since we don't save each channel when we set it's position, the channel count will remain the same throughout the execution of this function.
+        // So for the rest of the channels, if any, we increment the parentPositions[id], and use it to calculate the new position of the next channel
+        const parentPositions: Record<string, number> = {};
+
+        const missingPosition = channelData.filter((channel) => {
+            return Object.prototype.hasOwnProperty.call(channel, "parent") && !Object.prototype.hasOwnProperty.call(channel, "position");
+        });
+
+        for (const channel of missingPosition) {
+            const parent = channel.parent;
+            const parentPosition = parentPositions[parent];
+
+            const childrenChannelsCount = serverChannels.filter(
+                (channel) => (channel.parent || null) === parent && channel.type !== ChannelTypes.SERVER_CATEGORY
+            ).length;
+
+            if (!parentPosition) {
+                parentPositions[parent] = 1;
+
+                channel.position = childrenChannelsCount;
+            } else {
+                channel.position = childrenChannelsCount + parentPositions[parent];
+
+                parentPositions[parent]++;
+            }
         }
 
         const updatedChannelDocuments = affectedChannels.map((serverChannel) => {
             const updatedChannelData = channelData.find((channel) => channel.id === serverChannel.id);
 
             if (updatedChannelData) {
-                serverChannel.position = updatedChannelData.position;
+                if (Object.prototype.hasOwnProperty.call(updatedChannelData, "position")) {
+                    serverChannel.position = updatedChannelData.position;
+                }
+
+                if (Object.prototype.hasOwnProperty.call(updatedChannelData, "parent")) {
+                    serverChannel.parent = updatedChannelData.parent;
+                }
             }
 
             return serverChannel;
         });
 
-        const positionArray = updatedChannelDocuments.map((updatedChannel) => updatedChannel.position);
-        const overlappingPositions = positionArray.some((position, index) => {
-            return positionArray.indexOf(position, index + 1) !== -1;
-        });
+        const parentToChannels = serverChannels.reduce((accumulator, channel) => {
+            const parent = channel.parent || (channel.type === 3 && "category") || "orphan";
 
-        if (overlappingPositions) {
-            return next(new GenericError("Some of the positions are overlapping"));
+            if (!accumulator[parent]) { accumulator[parent] = []; }
+
+            accumulator[parent].push(channel);
+
+            return accumulator;
+        }, {} as Record<string, IChannelDocument[]>);
+
+        for (const parent of Object.keys(parentToChannels)) {
+            const positionArray = parentToChannels[parent].map((channel) => channel.position);
+
+            const overlappingPositions = positionArray.some((position, index) => {
+                return positionArray.indexOf(position, index + 1) !== -1;
+            });
+
+            if (overlappingPositions) {
+                return next(new GenericError("Some of the positions are overlapping"));
+            }
         }
 
         try {
