6d30a400aa71e1eedff8153def8903a602d48c52
chris9740 <christofferhja@gmail.com>
2021-05-24 13:13:29 +0200

    refactor: split logic between DM messaging and server channel messaging

--- src/api/app/routes/channels/channel/messages/index.ts
+++ src/api/app/routes/channels/channel/messages/index.ts
@@ -2,18 +2,54 @@ import { Router } from "express";
 import MessagesController from "../../../../controllers/channels/channel/messages/MessagesController";
 import authenticate from "../../../../middlewares/authenticate";
 import checkServerPermissions from "../../../../middlewares/checkServerPermissions";
+import findChannel from "../../../../middlewares/buses/findChannel";
+import { ChannelTypes } from "../../../../../../util/Constants";
 
 const router = Router();
 
 router.get("/",
-    authenticate({ required: true, allowApplications: true, scopes: ["servers.read"] }),
+    authenticate({ required: true }),
+    findChannel({
+        types: [
+            ChannelTypes.SERVER_TEXT
+        ],
+        passthrough: true
+    }),
     checkServerPermissions({ flag: "VIEW_MESSAGES" }),
     MessagesController.getMessages
 );
 
 router.post("/",
-    authenticate({ required: true, allowApplications: true, scopes: ["servers.read"] }),
+    authenticate({ required: true }),
+    findChannel({
+        types: [
+            ChannelTypes.SERVER_TEXT
+        ],
+        passthrough: true
+    }),
     checkServerPermissions({ flag: "SEND_MESSAGES" }),
+    MessagesController.createMessage
+);
+
+router.get("/",
+    authenticate({ required: true }),
+    findChannel({
+        types: [
+            ChannelTypes.DM,
+            ChannelTypes.GROUP_DM
+        ]
+    }),
+    MessagesController.getMessages
+);
+
+router.post("/",
+    authenticate({ required: true }),
+    findChannel({
+        types: [
+            ChannelTypes.DM,
+            ChannelTypes.GROUP_DM
+        ]
+    }),
     MessagesController.createMessage
 );
 
